6099f23684d7e795cbe4565c4afe5717d6718205
Brian Timar <timarbrian@gmail.com>
2019-02-17 16:48:11 -0800

    added generator constructor

--- models.py
+++ models.py
@@ -1,7 +1,52 @@
 """ tf GAN models """
 
 class DCGAN:
-    """Imitating architecture from the original DCGAN paper
-        https://arxiv.org/pdf/1511.06434.pdf
-        """
-    def __init__(self, num_layers):
+    """DCGAN:  https://arxiv.org/pdf/1511.06434.pdf.
+    Here I'm borrowing the implementation from the TF tutorial (see readme)"""
+
+    def __init__(self):
+        pass
+
+    def build_generator(self, noise, output_shape=(64,64)):
+        """ build image generator based on sampling from the given noise
+        tensor"""
+
+        #how many conv layers to apply in upsampling to output
+        num_upsample=2
+        #how many channels to start with
+        init_channels=256
+        #pick strides to end up with correct output shape. Will be doubling
+        #at each transpose-conv
+        init_size = output_shape[0]//(2**num_upsample)
+        kernel_size=(5,5)
+
+        gen_model = tf.keras.Sequential()
+        gen_model.add(tf.keras.layers.Dense(init_size * init_size * init_channels,
+                                            use_bias=False,
+                                            input_shape=tf.shape(noise)))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.LeakyReLU())
+
+        model.add(tf.keras.layers.reshape((init_size, init_size, init_channels)))
+        channels=init_channels//2
+        model.add(tf.keras.layers.Conv2DTranspose(channels, kernel_size,
+                                        strides=(1,1),padding='same',
+                                        use_bias=False))
+
+        assert model.output_shape==(None, init_size, init_size, 128)
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.LeakyReLu())
+        #double size at each step
+        for ii in range(num_upsample-1):
+            channels = channels//2
+            model.add(tf.keras.layers.Conv2DTranspose(channels, kernel_size,
+                                            strides=(2,2)), padding='same',
+                                            use_bias=False)
+            model.add(tf.keras.layers.BatchNormalization())
+            model.add(tf.keras.layers.LeakyReLU())
+        # at the final step, collapse to a single channel, apply tanh
+        model.add(tf.keras.layers.Conv2DTranspose(1, kernel_size, strides=(2,2),
+                                                padding='same', use_bias=False,
+                                                activation='tanh'))
+        assert model.shape == (None, *output_shape, 1)
+        return model
--- readme.md
+++ readme.md
@@ -8,9 +8,13 @@
   * in the standard conv case, patches are drawn from the larger tensor, and single values placed into the smaller --> downsampling.
   * in the transposed conv, patches are drawn from the smaller tensor, and single values fed into the larger --> upsampling.
 
-In each case the frames can be zero-padded as necessary.
+  In each case the frames can be zero-padded as necessary.
+  One other thing worth noting about the transpose conv: the *stride* has a slightly different meaning in the transpose case than in the standard case. In a standard conv, it controls how far the patch steps across the input when the output pixel is shifted over by one. In the transpose case, the patch always steps by one, while the stride controls the spacing between non-zero-padding pixels in the input. Easiest to just look at the pictures.
+
+* [TF demo][tf DCGAN] of DCGAN training on MNIST
 
 [ganhacks]: https://github.com/soumith/ganhacks
 [catgan]: https://github.com/AlexiaJM/Deep-learning-with-cats
 [dcgan]: https://arxiv.org/pdf/1511.06434.pdf
 [conv vis]: https://github.com/vdumoulin/conv_arithmetic
+[tf DCGAN]: https://github.com/tensorflow/tensorflow/blob/r1.13/tensorflow/contrib/eager/python/examples/generative_examples/dcgan.ipynb
