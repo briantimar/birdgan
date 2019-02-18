63b376054b6543a3f26bef629f39d5bebb6de00f
Brian Timar <timarbrian@gmail.com>
2019-02-18 07:27:31 -0800

    typos

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ class DCGAN:
     def __init__(self):
         pass
 
-    def build_generator(self, noise, output_shape=(64,64)):
-        """ build image generator based on sampling from the given noise
-        tensor"""
+    def build_generator(self, input_shape, output_shape=(64,64)):
+        """ build image generator based on sampling from noise
+        tensor of given shape."""
 
         #how many conv layers to apply in upsampling to output
         num_upsample=2
@@ -21,14 +21,15 @@ class DCGAN:
         init_size = output_shape[0]//(2**num_upsample)
         kernel_size=(5,5)
 
-        gen_model = tf.keras.Sequential()
-        gen_model.add(tf.keras.layers.Dense(init_size * init_size * init_channels,
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(init_size * init_size * init_channels,
                                             use_bias=False,
-                                            input_shape=tf.shape(noise)))
+                                            input_shape=input_shape
+                                            ))
         model.add(tf.keras.layers.BatchNormalization())
         model.add(tf.keras.layers.LeakyReLU())
 
-        model.add(tf.keras.layers.reshape((init_size, init_size, init_channels)))
+        model.add(tf.keras.layers.Reshape((init_size, init_size, init_channels)))
         channels=init_channels//2
         model.add(tf.keras.layers.Conv2DTranspose(channels, kernel_size,
                                         strides=(1,1),padding='same',
@@ -36,18 +37,18 @@ class DCGAN:
 
         assert model.output_shape==(None, init_size, init_size, 128)
         model.add(tf.keras.layers.BatchNormalization())
-        model.add(tf.keras.layers.LeakyReLu())
+        model.add(tf.keras.layers.LeakyReLU())
         #double size at each step
         for ii in range(num_upsample-1):
             channels = channels//2
             model.add(tf.keras.layers.Conv2DTranspose(channels, kernel_size,
-                                            strides=(2,2)), padding='same',
-                                            use_bias=False)
+                                            strides=(2,2), padding='same',
+                                            use_bias=False))
             model.add(tf.keras.layers.BatchNormalization())
             model.add(tf.keras.layers.LeakyReLU())
         # at the final step, collapse to a single channel, apply tanh
         model.add(tf.keras.layers.Conv2DTranspose(1, kernel_size, strides=(2,2),
                                                 padding='same', use_bias=False,
                                                 activation='tanh'))
-        assert model.shape == (None, *output_shape, 1)
+        assert model.output_shape == (None, *output_shape, 1)
         return model
